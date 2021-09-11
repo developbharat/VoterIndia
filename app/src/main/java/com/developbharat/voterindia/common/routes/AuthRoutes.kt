e9a0dab6a76a6f7d07e3f10bb92cf98ad706d403
hat <dev.jayantmalik@gmail.com>
2021-09-11 21:19:06 +0530

    fix: fixed invalid routes to objects instead of classes

--- app/src/main/java/com/developbharat/voterindia/common/routes/AuthRoutes.kt
+++ app/src/main/java/com/developbharat/voterindia/common/routes/AuthRoutes.kt
@@ -1,10 +1,10 @@
 package com.developbharat.voterindia.common.routes
 
 sealed class AuthRoutes(val route: String){
-    class WelcomeScreen: AuthRoutes("auth_welcome_screen")
-    class MobileAuthScreen: AuthRoutes("auth_mobile_screen")
-    class SignupScreen: AuthRoutes("auth_signup_screen")
-    class MobilePasscodeScreen: AuthRoutes("auth_mobile_passcode_screen")
-    class RequestLicenceScreen: AuthRoutes("auth_request_licence_screen")
-    class LicenceActivationScreen: AuthRoutes("auth_licence_activation_screen")
+    object WelcomeScreen: AuthRoutes("auth_welcome_screen")
+    object MobileAuthScreen: AuthRoutes("auth_mobile_screen")
+    object SignupScreen: AuthRoutes("auth_signup_screen")
+    object MobilePasscodeScreen: AuthRoutes("auth_mobile_passcode_screen")
+    object RequestLicenceScreen: AuthRoutes("auth_request_licence_screen")
+    object LicenceActivationScreen: AuthRoutes("auth_licence_activation_screen")
 }
--- app/src/main/java/com/developbharat/voterindia/common/routes/MainRoutes.kt
+++ app/src/main/java/com/developbharat/voterindia/common/routes/MainRoutes.kt
@@ -1,6 +1,6 @@
 package com.developbharat.voterindia.common.routes
 
 sealed class MainRoutes(val route: String){
-    class DashboardScreen(): MainRoutes("main_dashboard_screen")
+    object DashboardScreen: MainRoutes("main_dashboard_screen")
 }
 
